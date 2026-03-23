c0a24511ea5d31f01b502a44094d4141d1874d8b
Zane-Jiang <2129056867@qq.com>
2026-03-23 13:17:10 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
-source benchmark/script/run_common.sh
-source benchmark/script/run_measure_latency.sh
 export OMP_NUM_THREADS=120
 
-pushd benchmark/XSBench/openmp-threading
 REBUILD=$1
+MODE=${2:-11111}   
+if [ "$MODE" == "latency" ]; then
+    source benchmark/script/run_measure_latency.sh
+else
+    source benchmark/script/run_common.sh
+fi
+
+pushd benchmark/XSBench/openmp-threading
+
 if [ $REBUILD -eq 1 ]; then
     echo "rebuilding...."
     make clean && make -j$(nproc) 
 fi
 
-MODE=${2:-111}   
-
 if [ "$MODE" == "latency" ]; then
-    run_and_measure_latency $(realpath ./XSBench) -m event -s XXL -g 800000 -p 20000000 -l 34
+    run_and_measure_latency $(realpath ./XSBench) -s XXL -g 800000 -p 20000000 -l 34
 else
-    run_and_analyze $MODE $(realpath ./XSBench) -m event -s XXL -g 800000 -p 20000000 -l 34
+    # For run_fourth (position 4), disable madvise
+    if [[ "$MODE" =~ ^.{4}1 ]]; then
+        echo "Running fourth pass with madvise disabled..."
+        export CXL_MALLOC_ENABLE_MADVISE=0
+    fi
+    run_and_analyze $MODE $(realpath ./XSBench) -s XL -g 200000 -p 20000000 -l 34 -t 120
+    # Clean up any madvise environment variable
+    unset CXL_MALLOC_ENABLE_MADVISE
 fi
-# run_and_analyze $MODE $(realpath ./XSBench) -m event -s XXL -l 34 -p 20000000 -G nuclide
 popd
