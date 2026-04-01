dc9550ec7afeba26944b22b7f9d85ac076399aef
Zane-Jiang <2129056867@qq.com>
2026-04-01 14:25:47 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -3,29 +3,15 @@ export OMP_NUM_THREADS=120
 
 REBUILD=$1
 MODE=${2:-11111}   
-if [ "$MODE" == "latency" ]; then
-    source benchmark/script/run_measure_latency.sh
-else
-    source benchmark/script/run_common.sh
-fi
+
+source benchmark/script/run_common.sh
+
 
 pushd benchmark/XSBench/openmp-threading
-
 if [ $REBUILD -eq 1 ]; then
     echo "rebuilding...."
     make clean && make -j$(nproc) 
 fi
-
-if [ "$MODE" == "latency" ]; then
-    run_and_measure_latency $(realpath ./XSBench) -s XXL -g 800000 -p 20000000 -l 34
-else
-    # For run_fourth (position 4), disable madvise
-    if [[ "$MODE" =~ ^.{4}1 ]]; then
-        echo "Running fourth pass with madvise disabled..."
-        export CXL_MALLOC_ENABLE_MADVISE=0
-    fi
-    run_and_analyze $MODE $(realpath ./XSBench) -s XL -g 200000 -p 20000000 -l 34 -t 120
-    # Clean up any madvise environment variable
-    unset CXL_MALLOC_ENABLE_MADVISE
+run_and_analyze $MODE $(realpath ./XSBench) -s XL -g 200000 -p 20000000 -l 34 -t 120
 fi
 popd
